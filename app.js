80fd144e63b3e9efd015032545ac8a8504db6fe5
Henley <henley@berkeley.edu>
2023-01-28 00:23:19 -0800

    new hero

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+const body = document.body;
+const header = document.querySelector("nav");
+const main = document.querySelector("body");
+const headerHeight = document.querySelector("nav").offsetHeight;
+
+main.style.top = headerHeight + "px";
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    let currentScroll = window.pageYOffset;
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("newhome");
+    var xvalue = "center";
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " -" + yvalue + "px";
+    console.log("current: ", currentScroll);
+    console.log("last: ", lastScroll);
+    if(window.location.pathname == "/arts.html"){
+        return
+    }
+    if (currentScroll - lastScroll > 0 && currentScroll > window.innerWidth / 2 - 90) {
+        // scrolled down -- header hide
+        header.classList.add("scroll-down");
+        header.classList.remove("scroll-up");
+    } else if (currentScroll - lastScroll < 0 && currentScroll < window.innerWidth / 2 - 90){
+        // scrolled up -- header show
+        header.classList.add("scroll-up");
+        header.classList.remove("scroll-down");
+    }
+
+    lastScroll = currentScroll;
+    // console.log("last: ", lastScroll);
+
+})
+
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
@@ -21,3 +58,13 @@ moreclass.addEventListener('click',
     function(){
         moreclass.classList.toggle('open');
 });
+
+function parallax() {
+    var s = document.getElementById("floater");
+  var yPos = 0 - window.pageYOffset/5;  
+  s.style.top = 50 + yPos + "%"; }
+
+window.addEventListener("scroll", function(){
+    parallax(); 
+});
+
